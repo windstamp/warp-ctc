98b54996f653981f93ea4bd8b4702af9296997e9
Keliang Zhang <keliangzhang@KeliangdeMacBook-Pro.local>
2021-04-05 09:43:19 +0800

    bugfix for can not find gpuStream_t defintion

--- include/detail/reduce.h
+++ include/detail/reduce.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include "type_defs.h"
+
 template <typename T>
 ctcStatus_t reduce_negate(const T* input, T* output, int rows, int cols, bool axis, gpuStream_t stream);
 template <typename T>
